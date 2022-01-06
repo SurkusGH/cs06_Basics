800461e94ab6d105e3b8600c6ca2cc29944f8c29
Surkus <v.surkus@me.com>
2022-01-06 18:20:05 +0200

    Initial commit

---
+++ cs06_paskaita/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cs06_paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs06_paskaita!");
+
+        }
+    }
+}
+
