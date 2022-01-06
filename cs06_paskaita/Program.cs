5c8289fccf448f0039c40c9623173805c46491a4
juliusreea <97091256+juliusreea@users.noreply.github.com>
2022-01-06 19:15:38 +0200

    Update Program.cs

--- cs06_paskaita/Program.cs
+++ cs06_paskaita/Program.cs
@@ -8,6 +8,14 @@ namespace cs06_paskaita
         {
             Console.WriteLine("cs06_paskaita!");
 
+            Console.WriteLine("zemes skritulio plotas");
+
+            double r = 6371;
+
+            double res1 = 4 * Math.PI * Math.Pow(6371, 2);
+
+            Console.WriteLine(res1);
+
         }
     }
 }
