02d2f68c3c098ca05189d00dd6557faa0c7c1b12
Surkus <v.surkus@me.com>
2022-01-06 19:25:44 +0200

    Update Program.cs

--- cs06_paskaita/Program.cs
+++ cs06_paskaita/Program.cs
@@ -6,23 +6,14 @@ namespace cs06_paskaita
     {
         static void Main(string[] args)
         {
+            //Artūras
             double res2 = 0;
 
             res2 = (double)(4.0 / 3 * Math.PI * 6371 * 6371 * 6371);
 
             Console.WriteLine(res2);
-
-
-
-            double a = 10;
-            double b = 15;
-
-            double c = Math.Pow(a, 2) + Math.Pow(b, 2);
-            double res3 = Math.Sqrt(c);
-            Console.WriteLine(c);
-
-            Console.WriteLine($"4: {(res1 + res2 + res3) / 3}");
-
+            
+            //Julius
             Console.WriteLine("zemes skritulio plotas");
 
             double r = 6371;
@@ -30,6 +21,24 @@ namespace cs06_paskaita
             double res1 = 4 * Math.PI * Math.Pow(6371, 2);
 
             Console.WriteLine(res1);
+
+
+            //Vytautas
+            double a = 10;
+            double b = 15;
+
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2);
+            double res3 = Math.Sqrt(c);
+            Console.WriteLine(c);
+
+            Console.WriteLine("--- --- ---");
+
+            Console.WriteLine($"Kintamasis1 {res1}");
+            Console.WriteLine($"Kintamasis2 {res2}");
+            Console.WriteLine($"Kintamasis3 {res3}");
+
+            Console.WriteLine($"    Suma: {res1 + res2 + res3}");
+            Console.WriteLine($"Vidurkis: {(res1 + res2 + res3) / 3}");
 
         }
     }
